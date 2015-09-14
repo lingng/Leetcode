48049dee5b9c5baf9ad20a67bd859e53307cc44a
YutingZhang <lizlingng@gmail.com>
2015-09-14 14:30:36 -0700

    Python-034: Search for a range.

---
+++ python/34_search_for_a_range.py
@@ -0,0 +1,40 @@
+# https://leetcode.com/problems/search-for-a-range/
+
+# Given a sorted array of integers, find the starting and ending position of a given target value.
+# Your algorithm's runtime complexity must be in the order of O(log n).
+# If the target is not found in the array, return [-1, -1].
+# For example,
+# Given [5, 7, 7, 8, 8, 10] and target value 8,
+# return [3, 4].
+
+class Solution(object):
+    def searchRange(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        left = 0
+        right = len(nums) - 1
+        while left <= right:
+            mid = (left + right) / 2
+            if nums[mid] > target:
+                right = mid - 1
+            elif nums[mid] < target:
+                left = mid + 1
+            else:
+                rst = [0, 0]
+                if nums[left] == target: 
+                    rst[0] = left
+                if nums[right] == target: 
+                    rst[1] = right
+                for i in range(mid, right+1):
+                    if nums[i] != target: 
+                        rst[1] = i - 1
+                        break
+                for i in range(mid, left-1, -1):
+                    if nums[i] != target: 
+                        rst[0] = i + 1
+                        break
+                return rst
+        return [-1, -1]
