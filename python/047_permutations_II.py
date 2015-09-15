69efe28ddd8b9cc19baa54fcdb5def4543336dfe
YutingZhang <lizlingng@gmail.com>
2015-09-15 10:12:35 -0700

    Python-047: Add comment.

--- python/047_permutations_II.py
+++ python/047_permutations_II.py
@@ -1,6 +1,11 @@
+# https://leetcode.com/problems/permutations-ii/
+
+# Given a collection of numbers that might contain duplicates, return all possible unique permutations.
+# For example,
+# [1,1,2] have the following unique permutations:
+# [1,1,2], [1,2,1], and [2,1,1].
+
 class Solution(object):
-
-
     def permuteUnique(self, nums):
         """
         :type nums: List[int]
@@ -10,6 +15,8 @@ class Solution(object):
             return []
         if len(nums) == 1:
             return [nums]
+            
+        # contains duplicate. but the order of the original list does not matter.
         nums.sort()
         result = []
         previousNum = None
