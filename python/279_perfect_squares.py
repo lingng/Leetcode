b94cb8b3ffeeadd40f0e5a6b41bf1a8472421c12
YutingZhang <lizlingng@gmail.com>
2015-09-19 17:12:36 -0700

    Python-279: Add comments.

--- python/279_perfect_squares.py
+++ python/279_perfect_squares.py
@@ -5,7 +5,7 @@
 
 class Solution(object):
     _dp = [0]
-    
+
     # Ref: https://leetcode.com/discuss/56993/static-dp-c-12-ms-python-172-ms-ruby-384-ms
     def numSquares(self, n):
         """
@@ -17,6 +17,28 @@ class Solution(object):
             dp += min(dp[-i*i] for i in range(1, int(len(dp)**0.5+1))) + 1,
         return dp[n]
 
+    # A time limit exceed approach. But easier to understand.
+    def numSquares_TLE(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        # Or we can allocate an array with MAX_INT.
+        dp = collections.defaultdict(int)
+        y = 1
+        while y * y <= n:
+            # if the number is a square of y, then we can return 1. Eg, dp[1] = 1, dp[4] = 1, dp[9] = 1
+            dp[y * y] = 1
+            y += 1
+        for x in range(1, n + 1):
+            y = 1
+            while x + y * y <= n:
+                if x + y * y not in dp or dp[x] + 1 < dp[x + y * y]:
+                    # x + y^2 = dp[x] + 1. This 1 is for the y^2.
+                    dp[x + y * y] = dp[x] + 1
+                y += 1
+        return dp[n]
+        
 # Greedy won't work.
 # Eg, n = 12. Correct answer: 3. Greedy: 9 + 2 + 1 + 1 -> 4.
 
