9af8bcf9bc7350e09d9f136b699fe7508be00c8c
YutingZhang <lizlingng@gmail.com>
2015-09-30 15:18:06 -0700

    Python-190: Reverse bits.

---
+++ python/190_reverse_bits.py
@@ -0,0 +1,25 @@
+# https://leetcode.com/problems/reverse-bits/
+
+# Reverse bits of a given 32 bits unsigned integer.
+# For example, given input 43261596 (represented in binary as 00000010100101000001111010011100), return 964176192 (represented in binary as 00111001011110000010100101000000).
+# Follow up:
+# If this function is called many times, how would you optimize it?
+
+class Solution(object):
+    def reverseBits(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        rst = ""
+        while n > 0:
+          rst = rst + str(n%2)
+          n = n / 2
+        print rst
+        while len(rst)<32:
+          rst = rst + "0"
+        print rst
+        return int(rst, 2)
+        
+x = Solution()
+print x.reverseBits(1)
