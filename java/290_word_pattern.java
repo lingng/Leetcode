1d0685e3615c01117c1d6cd223ed912c8838e85b
YutingZhang <lizlingng@gmail.com>
2015-10-09 20:02:36 -0700

    Java-290: add solution.

--- java/290_word_pattern.java
+++ java/290_word_pattern.java
@@ -8,5 +8,48 @@ public class Solution {
             if (!Objects.equals(index.put(pattern.charAt(i), i),index.put(words[i], i)))
                 return false;
         return true;
-  }
+    }
+    
+    public boolean wordPattern1(String pattern, String str){
+        String[] words = str.split(" ");
+        if (pattern.length() != words.length){
+            return false;
+        }
+        if (pattern.length() == 0 || pattern.length() == 1){
+            return true;
+        }
+
+        HashMap< Character , String> dict = new HashMap<Character, String>();
+        for (int i = 0; i < pattern.length(); i++){
+            String tmp = dict.get(pattern.charAt(i));
+            if (tmp != null){
+                if (!words[i].equals(tmp)){
+                    return false;
+                }
+            }
+            else{
+                dict.put(pattern.charAt(i), words[i]);
+            }
+        }
+        dict.clear();
+        
+        HashMap<String, Character> dict1 = new HashMap<String, Character>();
+        for (int i = 0; i < pattern.length(); i++){
+            Character tmp = dict1.get(words[i]);
+            if (tmp != null){
+                if (pattern.charAt(i) != tmp){
+                    return false;
+                }
+            }
+            else{
+                dict1.put(words[i], pattern.charAt(i));
+            }
+        }
+        return true;
+    }
+    
+    public static void main(String[] args){
+        word_pattern w1 = new word_pattern();
+        System.out.println(w1.wordPattern("abba", "dog cat cat dog"));
+    }
 }
