ab4b4eae4246d7712b25ec543d6c7c0e4ab5e46a
YutingZhang <lizlingng@gmail.com>
2015-11-06 15:09:52 -0800

    Python-106: Construct binary tree from inorder and postorder traversal

---
+++ python/106_construct_binary_tree_from_inorder_and_post_order_traversal.py
@@ -0,0 +1,26 @@
+# https://leetcode.com/problems/construct-binary-tree-from-inorder-and-postorder-traversal/
+
+# Given inorder and postorder traversal of a tree, construct the binary tree.
+# Note:
+# You may assume that duplicates do not exist in the tree.
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def buildTree(self, inorder, postorder):
+        """
+        :type inorder: List[int]
+        :type postorder: List[int]
+        :rtype: TreeNode
+        """
+        if inorder:
+            ind = inorder.index(postorder.pop())
+            root = TreeNode(inorder[ind])
+            root.right = self.buildTree(inorder[ind+1:], postorder)
+            root.left = self.buildTree(inorder[:ind], postorder)
+            return root
