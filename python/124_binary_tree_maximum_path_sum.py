298f3942108fe811a5e5d2af4c31cfe6c92f8c0c
YutingZhang <lizlingng@gmail.com>
2015-11-07 15:27:24 -0800

    Python-124: Add test tree

--- python/124_binary_tree_maximum_path_sum.py
+++ python/124_binary_tree_maximum_path_sum.py
@@ -1,9 +1,9 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
 
 class Solution(object):
     def maxPathSum(self, root):
@@ -16,4 +16,16 @@ class Solution(object):
         vLeft = self.dfs(root.left)
         vRight = self.dfs(root.right)
         self.maxSum = max(self.maxSum, vLeft+vRight+root.val)
+        # print self.maxSum
+        # print max(root.val+vLeft, root.val+vRight, 0)
         return max(root.val+vLeft, root.val+vRight, 0)
+
+root = TreeNode(1)
+root.left = TreeNode(2)
+root.right = TreeNode(3)
+root.left.left = TreeNode(10)
+root.left.right = TreeNode(20)
+root.right.left = TreeNode(4)
+root.right.right = TreeNode(5)
+x = Solution()
+x.maxPathSum(root)
