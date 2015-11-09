45fb958a2f9e12c1091443063b4f144e51a63011
YutingZhang <lizlingng@gmail.com>
2015-11-09 10:35:00 -0800

    Add solution

--- python/301_remove_invalid_parentheses.py
+++ python/301_remove_invalid_parentheses.py
@@ -38,5 +38,52 @@ class Solution(object):
         print visited
         return dfs(s)
 
+    def removeInvalidParentheses1(self, s):
+        """
+        :type s: str
+        :rtype: List[str]
+        """
+        stack = self.checkValid(s)
+        if len(stack) == 0:
+            return [s]
+        result = []
+        visited = set([s])   
+        p = 0
+        self.dfs(s, stack, p, result, visited)
+        return result
+
+
+    def dfs(self, s, stack, p, result, visited):
+        for i in range(0, len(s)):
+            if p >= len(stack):
+                return
+            if s[i] == stack[p]:
+                substr = s[:i]+s[i+1:]
+                if substr not in visited:
+                    visited.add(substr)
+                    tmp = self.checkValid(substr)
+                    if len(tmp) == 0:
+                        if substr not in result:
+                            result.append(substr)
+                    elif len(tmp) > len(stack):
+                        continue
+                    else:
+                        self.dfs(substr, stack, p+1, result, visited)
+
+        
+    def checkValid(self, s):
+        stack = []
+        for i in range(0, len(s)):
+            if s[i] == '(':
+                stack.append('(')
+            if s[i] == ')':
+                if len(stack) == 0:
+                    stack.append(')')
+                elif stack[-1] == '(':
+                    stack.pop()
+                else:
+                    stack.append(')')
+        return stack
+
 x = Solution()
 print x.removeInvalidParentheses("(((k()((")
